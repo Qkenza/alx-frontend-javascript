322f90fc1c4fa80f9e8120444ba0979bb9110b89
Qkenza <kmerouar@gmail.com>
2024-10-01 01:21:01 +0000

    Es6

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,12 @@
+import getBudgetObject from './7-getBudgetObject.js';  // Added .js extension to correctly specify the module
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+
+  return fullBudget;
+}
